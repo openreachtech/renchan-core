afc1e49b7cc294331bcdfc13e3b60a34c7b3e882
Stew Eucen <stew.eucen@gmail.com>
2025-03-24 12:33:38 +0900

    Fix type error in test for BaseGraphqlContext#get:Ctor

--- tests/__tests__/server/graphql/contexts/BaseGraphqlContext.js
+++ tests/__tests__/server/graphql/contexts/BaseGraphqlContext.js
@@ -1142,6 +1142,32 @@ describe('BaseGraphqlContext', () => {
     /** @type {ExpressType.Request} */
     const expressRequestMock = /** @type {*} */ ({})
 
+    const queryMock = `
+      subscription OnObserveChatStates ($input: OnObserveChatStatesInput!) {
+        onObserveChatStates (input: $input) {
+          hasUnreadMessages
+          isUpdatedMembers
+        }
+      }
+    `
+
+    /** @type {GraphqlType.WebSocketRequestParams} */
+    const requestParamsMock = {
+      id: '98765432-abcd-0000-1234-999999999999',
+      type: 'subscribe',
+      payload: {
+        query: queryMock,
+        variables: {
+          roomId: 9999999,
+        },
+        context: {
+          headers: {
+            'x-renchan-access-token': 'omega',
+          },
+        },
+      },
+    }
+
     /** @type {GraphqlType.ServerEngine} */
     const engineMock = /** @type {*} */ ({})
 
@@ -1171,6 +1197,7 @@ describe('BaseGraphqlContext', () => {
       test.each(cases)('userId: $params.userEntity.id', ({ params }) => {
         const context = new BaseGraphqlContext({
           expressRequest: expressRequestMock,
+          requestParams: requestParamsMock,
           engine: engineMock,
           userEntity: params.userEntity,
           visa: visaMock,
