8cc08dcd2445e2bcb7d84c4a0c80e9ffa8d55794
Stew Eucen <stew.eucen@gmail.com>
2025-03-24 13:05:58 +0900

    Fix type error in test for BaseGraphqlContext#get:now

--- tests/__tests__/server/graphql/contexts/BaseGraphqlContext.js
+++ tests/__tests__/server/graphql/contexts/BaseGraphqlContext.js
@@ -2151,6 +2151,32 @@ describe('BaseGraphqlContext', () => {
       StubExpressRequest.create()
     )
 
+    const queryMock = `
+      subscription OnObserveChatStates ($input: OnObserveChatStatesInput!) {
+        onObserveChatStates (input: $input) {
+          hasUnreadMessages
+          isUpdatedMembers
+        }
+      }
+    `
+
+    /** @type {GraphqlType.WebSocketRequestParams} */
+    const requestParamsMock = {
+      id: '98765432-abcd-0000-1234-999999999999',
+      type: 'subscribe',
+      payload: {
+        query: queryMock,
+        variables: {
+          roomId: 9999999,
+        },
+        context: {
+          headers: {
+            'x-renchan-access-token': 'omega',
+          },
+        },
+      },
+    }
+
     const visaMock = GraphqlVisa.create()
 
     /**
@@ -2183,6 +2209,7 @@ describe('BaseGraphqlContext', () => {
 
       const args = {
         expressRequest: expressRequestMock,
+        requestParams: requestParamsMock,
         engine,
         userEntity: null,
         visa: visaMock,
