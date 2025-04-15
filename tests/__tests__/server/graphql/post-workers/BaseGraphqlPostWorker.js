1c438045750af0cc3931381d632358cb75713b89
Stew Eucen <stew.eucen@gmail.com>
2025-04-15 21:31:36 +0900

    Add test for BaseGraphqlPostWorker

---
+++ tests/__tests__/server/graphql/post-workers/BaseGraphqlPostWorker.js
@@ -0,0 +1,154 @@
+import BaseGraphqlPostWorker from '../../../../../lib/server/graphql/post-workers/BaseGraphqlPostWorker.js'
+
+import CustomerGraphqlServerEngine from '../../../../../app/server/graphql/CustomerGraphqlServerEngine.js'
+import AdminGraphqlServerEngine from '../../../../../app/server/graphql/AdminGraphqlServerEngine.js'
+
+describe('BaseGraphqlPostWorker', () => {
+  describe('constructor', () => {
+    describe('to keep properties', () => {
+      const cases = [
+        {
+          params: {
+            Engine: CustomerGraphqlServerEngine,
+          },
+        },
+        {
+          params: {
+            Engine: AdminGraphqlServerEngine,
+          },
+        },
+      ]
+
+      test.each(cases)('Engine: $params.Engine.name', async ({ params }) => {
+        const engine = await params.Engine.createAsync()
+
+        const postWorker = new BaseGraphqlPostWorker({
+          engine,
+        })
+
+        expect(postWorker)
+          .toHaveProperty('engine', engine)
+      })
+    })
+  })
+})
+
+describe('BaseGraphqlPostWorker', () => {
+  describe('.create()', () => {
+    describe('to be instance of own class', () => {
+      const cases = [
+        {
+          params: {
+            Engine: CustomerGraphqlServerEngine,
+          },
+        },
+        {
+          params: {
+            Engine: AdminGraphqlServerEngine,
+          },
+        },
+      ]
+
+      test.each(cases)('Engine: $params.Engine.name', async ({ params }) => {
+        const engine = await params.Engine.createAsync()
+
+        const postWorker = BaseGraphqlPostWorker.create({
+          engine,
+        })
+
+        expect(postWorker)
+          .toBeInstanceOf(BaseGraphqlPostWorker)
+      })
+    })
+
+    describe('to call constructor', () => {
+      const cases = [
+        {
+          params: {
+            Engine: CustomerGraphqlServerEngine,
+          },
+        },
+        {
+          params: {
+            Engine: AdminGraphqlServerEngine,
+          },
+        },
+      ]
+
+      test.each(cases)('Engine: $params.Engine.name', async ({ params }) => {
+        const engine = await params.Engine.createAsync()
+
+        const tallyArgs = {
+          engine,
+        }
+
+        const SpyClass = globalThis.constructorSpy.spyOn(BaseGraphqlPostWorker)
+
+        SpyClass.create(tallyArgs)
+
+        expect(SpyClass.__spy__)
+          .toHaveBeenCalledWith(tallyArgs)
+      })
+    })
+  })
+})
+
+describe('BaseGraphqlPostWorker', () => {
+  describe('.createAsync()', () => {
+    describe('to be instance of own class', () => {
+      const cases = [
+        {
+          params: {
+            Engine: CustomerGraphqlServerEngine,
+          },
+        },
+        {
+          params: {
+            Engine: AdminGraphqlServerEngine,
+          },
+        },
+      ]
+
+      test.each(cases)('Engine: $params.Engine.name', async ({ params }) => {
+        const engine = await params.Engine.createAsync()
+
+        const postWorker = await BaseGraphqlPostWorker.createAsync({
+          engine,
+        })
+
+        expect(postWorker)
+          .toBeInstanceOf(BaseGraphqlPostWorker)
+      })
+    })
+
+    describe('to call constructor', () => {
+      const cases = [
+        {
+          params: {
+            Engine: CustomerGraphqlServerEngine,
+          },
+        },
+        {
+          params: {
+            Engine: AdminGraphqlServerEngine,
+          },
+        },
+      ]
+
+      test.each(cases)('Engine: $params.Engine.name', async ({ params }) => {
+        const engine = await params.Engine.createAsync()
+
+        const tallyArgs = {
+          engine,
+        }
+
+        const SpyClass = globalThis.constructorSpy.spyOn(BaseGraphqlPostWorker)
+
+        await SpyClass.createAsync(tallyArgs)
+
+        expect(SpyClass.__spy__)
+          .toHaveBeenCalledWith(tallyArgs)
+      })
+    })
+  })
+})
