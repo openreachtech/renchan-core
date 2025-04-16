c3820ea93ec079d505a7e296507b9d9d0fa486a7
Stew Eucen <stew.eucen@gmail.com>
2025-04-16 14:28:26 +0900

    Declare CustomersQueryPostWorker below admin/

---
+++ app/server/graphql/post-workers/admin/queries/CustomersQueryPostWorker.js
@@ -0,0 +1,67 @@
+import BaseGraphqlPostWorker from '../../../../../../lib/server/graphql/post-workers/BaseGraphqlPostWorker.js'
+
+const Timber = console
+
+/**
+ * SignIn Mutation Post Worker
+ *
+ * @extends {BaseGraphqlPostWorker<
+ *   CustomersQueryPostWorkerVariables,
+ *   CustomersQueryPostWorkerContext,
+ *   CustomersQueryPostWorkerOutput
+ * >}
+ */
+export default class CustomersQueryPostWorker extends BaseGraphqlPostWorker {
+  /** @override */
+  static get schema () {
+    return 'customers'
+  }
+
+  /**
+   * On resolved hook.
+   *
+   * @override
+   * @param {{
+   *   variables: CustomersQueryPostWorkerVariables
+   *   context: CustomersQueryPostWorkerContext
+   *   information: GraphqlType.ResolverInputInformation
+   *   response: {
+   *     output: CustomersQueryPostWorkerOutput | null
+   *     error: Error | null
+   *   }
+   * }} params - Parameters.
+   * @returns {Promise<void>}
+   */
+  async onResolved ({
+    variables,
+    context,
+    information,
+    response: {
+      output,
+      error,
+    },
+  }) {
+    Timber.log('I am the post worker of CustomersQuery', {
+      companyName: variables[0].companyName,
+      variables,
+      output,
+      error,
+    })
+  }
+}
+
+/**
+ * @typedef {{}} CustomersQueryPostWorkerVariables
+ */
+
+/**
+ * @typedef {import('../../../contexts/AdminGraphqlContext.js').default} CustomersQueryPostWorkerContext
+ */
+
+/**
+ * @typedef {Array<{
+ *   id: number
+ *   name: string
+ *   inviteCode: string
+ * }>} CustomersQueryPostWorkerOutput
+ */
