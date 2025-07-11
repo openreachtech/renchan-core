2cca21a631f0b1f79c2bcf7ac8bea90d1cb08bdf
Stew Eucen <stew.eucen@gmail.com>
2025-07-11 17:32:12 +0900

    Fulfill AppRestfulApiContext#findUser() with actual SQL ad-hoc

--- app/server/restfulapi/contexts/AppRestfulApiContext.js
+++ app/server/restfulapi/contexts/AppRestfulApiContext.js
@@ -1,5 +1,9 @@
 import BaseRestfulApiContext from '../../../../lib/server/restfulapi/contexts/BaseRestfulApiContext.js'
 
+import Customer from '../../../sequelize/models/Customer.js'
+import CustomerBasic from '../../../sequelize/models/CustomerBasic.js'
+import CustomerAccessToken from '../../../sequelize/models/CustomerAccessToken.js'
+
 /**
  * App RESTful API context.
  *
@@ -40,10 +44,54 @@ export default class AppRestfulApiContext extends BaseRestfulApiContext {
     accessToken,
     requestedAt,
   }) {
-    return super.findUser({
-      expressRequest,
+    const customerAccessTokenEntity = await this.findCustomerAccessToken({
       accessToken,
     })
+
+    if (!customerAccessTokenEntity) {
+      return null
+    }
+
+    if (customerAccessTokenEntity.isExpired({
+      pointsAt: requestedAt,
+    })) {
+      return null
+    }
+
+    return customerAccessTokenEntity.Customer
+      ?? null
+  }
+
+  /**
+   * Find customer access token.
+   *
+   * @param {{
+   *   accessToken: string
+   * }} params - Parameters.
+   * @returns {Promise<import('../../../sequelize/models/CustomerAccessToken').CustomerAccessTokenAssociatedEntity | null>} - Customer access token.
+   */
+  static async findCustomerAccessToken ({
+    accessToken,
+  }) {
+    /** @type {import('../../../sequelize/models/CustomerAccessToken').CustomerAccessTokenAssociatedEntity | null} */
+    const customerAccessTokenEntity = /** @type {*} */ (
+      await CustomerAccessToken.findOne({
+        where: {
+          accessToken,
+        },
+        include: [
+          {
+            model: Customer,
+            include: [
+              CustomerBasic,
+            ],
+          },
+        ],
+      })
+    )
+
+    return customerAccessTokenEntity
+      ?? null
   }
 
   /**
