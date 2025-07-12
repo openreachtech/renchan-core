50743d5fd2185b18c7cd07857b808cc22074d24a
Stew Eucen <stew.eucen@gmail.com>
2025-07-13 03:22:15 +0900

    Change BasePostRenderer#get:preExpressHandlers to #buildPreExpressHandlers()

--- tests/__tests__/server/restfulapi/renderers/BaseRenderer.js
+++ tests/__tests__/server/restfulapi/renderers/BaseRenderer.js
@@ -594,7 +594,7 @@ describe('BaseRenderer', () => {
 })
 
 describe('BaseRenderer', () => {
-  describe('#get:preExpressHandlers', () => {
+  describe('#buildPreExpressHandlers()', () => {
     describe('to fixed value', () => {
       const cases = [
         {
@@ -637,14 +637,14 @@ describe('BaseRenderer', () => {
 
       test.each(cases)('errorStructureHash: $params.errorStructureHash', ({ params }) => {
         const expected = []
-        const tally = BaseRenderer.preExpressHandlers
+        const tally = BaseRenderer.buildPreExpressHandlers()
 
         const args = {
           errorStructureHash: params.errorStructureHash,
         }
         const renderer = BaseRenderer.create(args)
 
-        const actual = renderer.preExpressHandlers
+        const actual = renderer.buildPreExpressHandlers()
 
         expect(actual)
           .toEqual(expected)
