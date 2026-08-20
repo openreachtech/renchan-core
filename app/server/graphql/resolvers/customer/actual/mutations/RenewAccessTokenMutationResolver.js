37675a096bbe3f90394033dc34b3fdcecc084ead
Stew Eucen <stew.eucen@gmail.com>
2026-08-20 17:44:22 +0900

    Rename callback variable name to others

--- app/server/graphql/resolvers/customer/actual/mutations/RenewAccessTokenMutationResolver.js
+++ app/server/graphql/resolvers/customer/actual/mutations/RenewAccessTokenMutationResolver.js
@@ -57,11 +57,11 @@ export default class RenewAccessTokenMutationResolver extends BaseMutationResolv
     }
 
     // save renew access token
-    const callback = this.generateTransactionCallback({
+    const transactionHandler = this.generateTransactionCallback({
       customerId: accessTokenEntity.CustomerId,
       now: context.now,
     })
-    const renewedAccessTokenEntity = await CustomerAccessToken.beginTransaction(callback)
+    const renewedAccessTokenEntity = await CustomerAccessToken.beginTransaction(transactionHandler)
 
     return this.formatResponse({
       accessTokenEntity: renewedAccessTokenEntity,
--- app/server/graphql/resolvers/customer/actual/mutations/SendChatMessageMutationResolver.js
+++ app/server/graphql/resolvers/customer/actual/mutations/SendChatMessageMutationResolver.js
@@ -38,12 +38,12 @@ export default class SendChatMessageMutationResolver extends BaseMutationResolve
       postedAt: context.now,
     }
 
-    const callback = await this.generateTransactionCallback({
+    const transactionHandler = await this.generateTransactionCallback({
       attributeHash,
     })
 
     /** @type {import('../../../../../../sequelize/models/ChatMessage.js').ChatMessageEntity | null} */
-    const result = await ChatMessage.beginTransaction(callback)
+    const result = await ChatMessage.beginTransaction(transactionHandler)
 
     if (!result) {
       throw new this.Error.FailToSave()
