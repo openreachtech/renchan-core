03f9a14f674d500df9ce258a517fd89e17a73404
Stew Eucen <12193670+StewEucen@users.noreply.github.com>
2026-09-20 11:29:21 +0900

    Add expect.deepContaining() to the test environment

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 export default {
   setupFilesAfterEnv: [
+    '@openreachtech/jest-deep-containing/lib/setup-expect-deepContaining.js',
     '<rootDir>/tests/setup-after-env.js',
   ],
   moduleNameMapper: {
